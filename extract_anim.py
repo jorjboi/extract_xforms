93d660f4173caaa4e9e3b9b18abeaba05bccd88a
George Geng <george@Georges-MacBook.lan>
2025-06-19 20:22:33 -0700

    refactor functions

--- extract_anim.py
+++ extract_anim.py
@@ -1,9 +1,26 @@
 import hou
 
 
-def create_main_subnet(alembic_input):
+def create_main_subnet():
     subnet_node = hou.node("/obj").createNode("subnet", "extract_animation")
-    divide_geo_node = subnet_node.createNode("geo", "divide_into_parts")
+    subnet_node.moveToGoodPosition()
+    return subnet_node
 
-    obj_merge = divide_geo_node.createNode("object_merge", "merge_alembic")
-    obj_merge.parm('objpath1').set(alembic_input)
+
+def create_divide_into_parts_geo_node(parent_node):
+    divide_geo_node = parent_node.createNode("geo", "divide_into_parts")
+    divide_geo_node.moveToGoodPosition()
+    return divide_geo_node
+
+
+def divide_into_parts(parent_node, alembic_path):
+    obj_merge = parent_node.createNode("object_merge", "merge_alembic")
+    obj_merge.moveToGoodPosition()
+    obj_merge.parm('objpath1').set(alembic_path)
+    return obj_merge
+
+
+def main(alembic_path):
+    subnet_node = create_main_subnet()
+    divide_node = create_divide_into_parts_geo_node(subnet_node)
+    divide_into_parts(divide_node, alembic_path)
